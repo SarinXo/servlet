a5f66ef1fe989c5798f103f30123bf5926d26bcf
rollo <rollovitch.roll@yandex.ru>
2023-10-06 16:36:23 +0300

    +server

---
+++ src/main/java/servlets/MainServlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MainServlet", urlPatterns = "/my-servlet")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.getWriter().println("Jojo");
+    }
+
+}
